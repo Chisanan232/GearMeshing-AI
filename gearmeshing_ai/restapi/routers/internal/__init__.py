11481c9da08d3f10e3669e72140a62c6572d9be8
Chisanan232 <chi10211201@cycu.org.tw>
2026-02-01 20:39:15 +0800

    ♻️ Move the sub-package about the internal routers into the sub-pacakge about router to be more reasonable.

---
+++ gearmeshing_ai/restapi/routers/internal/__init__.py
@@ -0,0 +1,11 @@
+"""Internal utilities package for GearMeshing-AI REST API.
+
+This package contains internal utilities and helpers that support
+the REST API functionality, following duck typing principles.
+
+Note: Most utility functions have been moved to core/models/io/utils.py
+to create a cohesive I/O package. This package is reserved for
+truly internal utilities that don't directly relate to I/O models.
+"""
+
+__all__ = []
