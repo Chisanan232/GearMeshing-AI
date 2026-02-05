f7e964efad5411f309c1f6953855ab928c4843ea
Chisanan232 <chi10211201@cycu.org.tw>
2026-02-05 09:47:41 +0800

    ➕ Export the abstractions of the package.

--- gearmeshing_ai/agent_core/abstraction/__init__.py
+++ gearmeshing_ai/agent_core/abstraction/__init__.py
@@ -1,7 +1,16 @@
+from .settings import AgentSettings, ModelSettings
 from .adapter import AgentAdapter
+from .mcp import MCPClientAbstraction
 from .cache import AgentCache
+from .factory import AgentFactory
+from .env_manager import EnvManager
 
 __all__ = [
+    "AgentSettings",
+    "ModelSettings",
     "AgentAdapter",
+    "MCPClientAbstraction",
     "AgentCache",
+    "AgentFactory",
+    "EnvManager",
 ]
