2bebc7cc432e6b89b6d3da47ca9f9898cedc4d95
Chisanan232 <chi10211201@cycu.org.tw>
2026-02-21 13:04:38 +0800

    ➕ Export the setting model and its loading mechanism of scheduler features.

--- gearmeshing_ai/scheduler/config/__init__.py
+++ gearmeshing_ai/scheduler/config/__init__.py
@@ -7,3 +7,10 @@ Key Components:
 - Settings: Application settings with environment variable support
 - Loader: Configuration loading utilities
 """
+
+from .settings import get_scheduler_settings, scheduler_settings
+
+__all__ = [
+    "get_scheduler_settings",
+    "scheduler_settings",
+]
